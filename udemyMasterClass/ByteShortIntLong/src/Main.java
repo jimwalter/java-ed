649a9f68be9c803269c2278dfe542dbd3f7b9a80
jim walter <jimwalter13090@gmail.com>
2020-06-11 15:37:59 -0600

    Integer data type and overflow / underflow

---
+++ udemyMasterClass/ByteShortIntLong/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + -2_147_483_648);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+    }
+    
+}
