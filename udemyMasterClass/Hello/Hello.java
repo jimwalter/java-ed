2759d62ca0113f1227eb46e6a400e5247ca04535
jim walter <jimwalter13090@gmail.com>
2020-06-11 14:50:18 -0600

    lesson 3.23 checkpoint

--- udemyMasterClass/Hello/Hello.java
+++ udemyMasterClass/Hello/Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Jim");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNUmber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNUmber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne
     }
 }
