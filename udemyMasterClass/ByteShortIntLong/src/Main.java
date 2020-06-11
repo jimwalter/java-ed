88fbddfca2656395560d39dfcdda67b137e5a1be
jim walter <jimwalter13090@gmail.com>
2020-06-11 15:48:27 -0600

    Byte, Short, Long, memory

--- udemyMasterClass/ByteShortIntLong/src/Main.java
+++ udemyMasterClass/ByteShortIntLong/src/Main.java
@@ -2,10 +2,31 @@ public class Main {
     public static void main(String[] args) {
         int myValue = 10000;
 
+        // INTEGERS
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxIntValue = Integer.MAX_VALUE;
-        System.out.println("Integer Minimum Value = " + -2_147_483_648);
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        
+        // BYTEs
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+        
+        // SHORT
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("short Minimum Value = " + myMinShortValue);
+        System.out.println("short Maximum Value = " + myMaxShortValue);
+        
+        // LONG
+
+        long myLongValue = 100L;
+        System.out.println(myLongValue);
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
     }
-    
 }
