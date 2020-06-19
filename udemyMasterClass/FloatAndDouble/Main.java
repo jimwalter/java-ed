0692de3986cd65661e05f690c39056cb1dd7359a
jim walter <jimwalter13090@gmail.com>
2020-06-19 11:20:12 -0600

    float and double data type min's and max's

---
+++ udemyMasterClass/FloatAndDouble/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Minimum value = " + myMinFloatValue);
+        System.out.println("Float Max value = " + myMaxFloatValue);
+
+        double myMinDoubleVal = Double.MIN_VALUE;
+        double myMaxDoubleVal = Double.MAX_VALUE;
+        System.out.println("Double Min Val = " + myMinDoubleVal);
+        System.out.println("Double Max Val = " + myMaxDoubleVal);
+    }
+}
