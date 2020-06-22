6fdcaa96d0d4febb9301c783c01f1d5959fd5565
jim walter <jimwalter13090@gmail.com>
2020-06-22 14:31:09 -0600

    video solutions operator challenge

--- udemyMasterClass/Operators_challenge/Main.java
+++ udemyMasterClass/Operators_challenge/Main.java
@@ -2,11 +2,14 @@ public class Main {
     public static void main(String[] args) {
         double var1 = 20.00d;
         double var2 = 80.00d;
-        double result1 = (var1 + var2) * 100;
-
+        // all values need the same precision and 'd' for double expression
+        double result1 = (var1 + var2) * 100.00d;
+        System.out.println("mytotals: " + result1);
+        
         double remainder1 = result1 % 40.00d;
-
+        
         boolean bool1 = (remainder1 == 0) ? true : false;
+        System.out.println("is no remainder? : " + bool1);
 
         System.out.println("was remainder 0: " + bool1);
 
