f1b135646fb9466cb44fe2b6425122e502bc809e
jim walter <jimwalter13090@gmail.com>
2020-06-22 15:47:14 -0600

    if else statement

---
+++ udemyMasterClass/Keywords&Exp/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 5000;
+
+        if (score <= 6000) {
+            System.out.println("Your score was less than 5000");
+
+        } else {
+            System.out.println("Got here");
+        }
+    }
+}
