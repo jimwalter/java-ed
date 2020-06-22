08f6b3fdf40a9bb5933217fb8439078f4d1cc477
jim walter <jimwalter13090@gmail.com>
2020-06-22 15:49:49 -0600

    if else if statement

--- udemyMasterClass/Keywords&Exp/Main.java
+++ udemyMasterClass/Keywords&Exp/Main.java
@@ -1,12 +1,16 @@
+import javax.swing.text.GapContent;
+
 public class Main {
     public static void main(String[] args) {
         int score = 5000;
 
-        if (score <= 6000) {
+        if (score <= 4000) {
             System.out.println("Your score was less than 5000");
 
+        } else if (score > 4500 && score < 6700) {
+            System.out.println("Score greater than 4500");
         } else {
-            System.out.println("Got here");
+            System.out.println("got to end");
         }
     }
 }
