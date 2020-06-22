7ca3dbb330ad9d12c4308384f14b69c0c22e6018
jim walter <jimwalter13090@gmail.com>
2020-06-22 16:07:15 -0600

    defined parameters in our method declaration, passed during invocation

--- udemyMasterClass/Methods/Main.java
+++ udemyMasterClass/Methods/Main.java
@@ -2,17 +2,14 @@ public class Main {
 
     //main is a method
     public static void main(String[] args) {
+        
         // invoke calculate score method
-        calculateScore();
+        calculateScore(true, 5000, 5, 100);
     }
-    // new method for calculating score
-    public static void calculateScore() {
-        int score = 5000;
-        int bonus = 100;
-        boolean gameover = true;
-        int levelCompleted = 5;
 
-        if (gameover) {
+    // new method for calculating score
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             System.out.println("You final score was : " + finalScore);
         }
