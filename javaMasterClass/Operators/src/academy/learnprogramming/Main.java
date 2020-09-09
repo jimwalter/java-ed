4095bf39b05b378874725a43b39a7e850aafc350
jim walter <jimwalter13090@gmail.com>
2020-09-09 14:27:33 -0600

    IF and code blocks discussed

--- javaMasterClass/Operators/src/academy/learnprogramming/Main.java
+++ javaMasterClass/Operators/src/academy/learnprogramming/Main.java
@@ -10,19 +10,25 @@ public class Main {
         System.out.println("1 + 2 = " + result);
         int previousResult = result;
         System.out.println("previousResult = " + previousResult);
+
         // subtraction
         result = result - 1;
         System.out.println("3 - 1 = " + result);
         System.out.println("previousResult = " + previousResult);
+
         // multiplication
         result = result * 10; // = 20;
         System.out.println(" 2 * 10 = " + result);
+
         // division
         result = result / 5;// =4
         System.out.println("20 / 5 = " + result);
+
         // modulo
         result = result % 3;
         System.out.println("4 % 3 = " + result);
+
+        /* Onto incrementation and operators on a variable */
 
         // increment
         result++;
@@ -30,7 +36,7 @@ public class Main {
 
         // decrement
         result--;
-        System.out.println("decrement res: " + result);
+        System.out.println("decrement result: " + result);
 
         // plus equals
         result += 2;
@@ -47,5 +53,13 @@ public class Main {
         // sub equals
         result -= 2;
         System.out.println(" 10 - 2: " + result);
+
+        /* onto if else */
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }
