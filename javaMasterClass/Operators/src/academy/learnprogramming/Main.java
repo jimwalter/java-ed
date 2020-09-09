4fcd97b15d46bcd50056c3e7f51c88b5cd067f8e
jim walter <jimwalter13090@gmail.com>
2020-09-09 14:21:40 -0600

    operators

---
+++ javaMasterClass/Operators/src/academy/learnprogramming/Main.java
@@ -0,0 +1,51 @@
+package academy.learnprogramming;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // addition
+        int result = 1 + 2; // = 3
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+        // subtraction
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult = " + previousResult);
+        // multiplication
+        result = result * 10; // = 20;
+        System.out.println(" 2 * 10 = " + result);
+        // division
+        result = result / 5;// =4
+        System.out.println("20 / 5 = " + result);
+        // modulo
+        result = result % 3;
+        System.out.println("4 % 3 = " + result);
+
+        // increment
+        result++;
+        System.out.println("increment result: " + result);
+
+        // decrement
+        result--;
+        System.out.println("decrement res: " + result);
+
+        // plus equals
+        result += 2;
+        System.out.println("plus equals 2 :" + result);
+
+        // times equals
+        result *= 10;
+        System.out.println("3 * 10 : " + result);
+
+        // div equals
+        result /= 3;
+        System.out.println("30 / 3 : " + result);
+
+        // sub equals
+        result -= 2;
+        System.out.println(" 10 - 2: " + result);
+    }
+}
